cd8277b80feb5f3c78c83c6d224208d48d205c01
Roman Bielokon <roman_slavyansk@mail.ru>
2016-08-16 09:38:39 +0300

    added test activity

---
+++ app/src/main/java/com/kvest/mvp_test/TestActivity.java
@@ -0,0 +1,16 @@
+package com.kvest.mvp_test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by roman on 8/16/16.
+ */
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity);
+    }
+}
