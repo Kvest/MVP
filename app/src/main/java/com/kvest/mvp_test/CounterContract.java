4452ec2f8414aebf1d3de98439bc368235f7fdd1
Roman Bielokon <roman_slavyansk@mail.ru>
2016-08-23 12:21:47 +0300

    added CounterContract

---
+++ app/src/main/java/com/kvest/mvp_test/CounterContract.java
@@ -0,0 +1,21 @@
+package com.kvest.mvp_test;
+
+import android.os.Bundle;
+
+import com.kvest.mvp.BasePresenter;
+import com.kvest.mvp.BaseView;
+
+/**
+ * Created by roman on 8/23/16.
+ */
+public class CounterContract {
+    interface View extends BaseView {
+        void setCounterValue(int value);
+        void logMessage(String message);
+    }
+
+    abstract class Presenter extends BasePresenter<View> {
+        abstract void increment();
+        abstract void decrement();
+    }
+}
